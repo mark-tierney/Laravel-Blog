8a2c6e7c4820447b7c43ea626000fcee929a9f65
mark-tierney <marktierney66@gmail.com>
2021-07-17 12:40:53 -0700

    setting up routes

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home.index');
+
+Route::get('/contact', function () {
+    return 'contact';
+})->name('home.contact');
+
+Route::get('/posts/{id}', function($id) {
+    return 'Blog post ' . $id;
+})->name('posts.show');
+
+Route::get('/recent-posts/{days_ago?}', function($daysAgo = 20) {
+    return 'Posts from ' . $daysAgo . ' days ago';
+})->name('posts.recent.index');
