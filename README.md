52d14fa650a3e41bc272f55204deb33e2f73a887
mark-tierney <marktierney66@gmail.com>
2021-08-06 15:46:10 -0700

    one to one profile and author tablesin db

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 This is a simple app implementing a CRUD in Laravel PHP for managing content on a MySQL database, developed by Mark L Tierney for demonstration purposed.
-
-Instructions for install:
-git clone https://github.com/mark-tierney/Laravel-CRUD.git
---
+++ app/Models/Profile.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+    //use BelongsTo;
+
+    public function author()
+    {
+        return $this->belongsTo('App\Models\Author');
+    }
+}
